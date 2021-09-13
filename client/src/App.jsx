9b17d802dc4f9afa3188e038b23568b98c6a75c5
Gilbert Zhuo <gilbertzhuo@gmail.com>
2021-09-13 21:04:52 +0800

    Implement List solidity contract

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {
+  newContextComponents
+} from "@drizzle/react-components";
 import "./App.css";
 
-export default function App (props){
+const {ContractData} = newContextComponents;
+export default function App ({drizzle, drizzleState}){
+  const [list, setList]  = useState();
+  const [data, setData] = useState();
+  useEffect(()=> {
+    const list_contract = drizzle.contracts.List;
+    
+
+  },[])
+  const submit = () => {
+      
+  }
   return (
     <>
-      <h1>Dapp Template</h1>
+      <h1>LIST</h1>
+      <ContractData drizzle={drizzle} drizzleState = {drizzleState} method="ListTotal" contract="List"/>
     </>
   )
 }
