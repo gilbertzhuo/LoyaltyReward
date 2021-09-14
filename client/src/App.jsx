f738bcc0753482bcc343cf7b778792d0bd25d1cb
Gilbert Zhuo <gilbertzhuo@gmail.com>
2021-09-14 20:07:32 +0800

    submit todo list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,59 @@
 import React, {useState, useEffect} from "react";
-import {
-  newContextComponents
-} from "@drizzle/react-components";
+
 import "./App.css";
 
-const {ContractData} = newContextComponents;
 export default function App ({drizzle, drizzleState}){
-  const [list, setList]  = useState();
-  const [data, setData] = useState();
-  useEffect(()=> {
-    const list_contract = drizzle.contracts.List;
-    
-
+  const [list, setList] = useState([]);
+  const [task, setTask] = useState("");
+  const [key, setKey] = useState("");
+  useEffect(() => {
+    const data = drizzle.contracts.List.methods.ViewList.cacheCall();
+    setKey(data);
   },[])
-  const submit = () => {
-      
+  function getData() {
+    const data = drizzleState.contracts.List.ViewList[key];
+    setList(data.value)
   }
+  function click() {
+    if (task.length > 0) {
+      setList([...list, task]);
+      drizzle.contracts.List.methods.AddItem(task).send()
+      setTask("");
+    }
+  }
+
+  function deleteItem(name) {
+    drizzle.contracts.List.methods.DeleteItem(name).send()
+    setList(list.filter(item => {
+      return item !== name}
+    ))
+  }
+
   return (
-    <>
-      <h1>LIST</h1>
-      <ContractData drizzle={drizzle} drizzleState = {drizzleState} method="ListTotal" contract="List"/>
-    </>
+    <div className="flex-center">
+      <div className="container">
+        <div className="flex-split">
+          <h1>Blockchain Task List</h1>
+          <button onClick={()=>getData()}>Get Data</button>
+        </div>
+        <div className="flex-split">
+          <input id="itemInput" placeholder="Type your task here..." onChange={(e)=>setTask(e.target.value)} value={task}/>
+          <button style={{fontWeight:"bolder"}} onClick={()=>click()}>ADD</button>
+        </div>
+        <div>
+        {list.length>0 ? (
+          (list.map((item,index) => (
+            <div key={index} className="flex-split item">
+              {item}
+              <div className="onHover" style={{background: "blanchedalmond", color:"red"}} onClick={()=>deleteItem(item)}>
+                x
+              </div>
+            </div>
+          )))
+        ):""}
+        </div>
+       
+      </div>
+    </div>
   )
 }
--- contracts/List.sol
+++ contracts/List.sol
@@ -6,23 +6,19 @@ contract List {
     constructor() {
         owner = msg.sender;
     }
-    event newItem(string name, string description);
+    event newItem(string name);
     event deleteItem(string name);
-    struct Item {
-        string name;
-        string description;
-    }
-    Item[] public ListTotal; 
-    function ViewList() external view returns(Item[] memory){
+    string[] public ListTotal; 
+    function ViewList() external view returns(string[] memory){
         return ListTotal;
     }
-    function AddItem(string calldata name, string calldata description) external{
-        ListTotal.push(Item(name,description));
-        emit newItem(name, description);
+    function AddItem(string calldata name) external{
+        ListTotal.push(name);
+        emit newItem(name);
     }
     function DeleteItem(string memory _name) public {
         for (uint i = 0; i < ListTotal.length; ++i) {
-            if (keccak256(abi.encodePacked(ListTotal[i].name)) ==  keccak256(abi.encodePacked(_name))) {
+            if (keccak256(abi.encodePacked(ListTotal[i])) ==  keccak256(abi.encodePacked(_name))) {
                 ListTotal[i] = ListTotal[ListTotal.length-1];
                 ListTotal.pop();
                 emit deleteItem(_name);
